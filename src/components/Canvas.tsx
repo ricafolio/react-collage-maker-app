202eb28f253419c122aea16d1bf62da504571df6
ricafolio <29043464+ricafolio@users.noreply.github.com>
2023-05-31 02:27:34 +0800

    fix: canvas ref type

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -3,7 +3,7 @@ import { useEffect, useRef } from "react";
 
 
 export default function Canvas() {
-  const canvasRef = useRef("")
+  const canvasRef = useRef<HTMLCanvasElement | null>(null)
   const CANVAS_WIDTH = 576
   const CANVAS_HEIGHT = 576
   const OBJECT_LOCKED = {
@@ -20,40 +20,42 @@ export default function Canvas() {
   }
 
   useEffect(() => {
-    // -> Setup canvas
-    const canvas = new fabric.Canvas(canvasRef.current, {
-      width: CANVAS_WIDTH,
-      height: CANVAS_HEIGHT,
-      backgroundColor: "#808080",
-      selection: false
-    })
+    if (canvasRef.current) {
+      // 1. Setup canvas
+      const canvas = new fabric.Canvas(canvasRef.current, {
+        width: CANVAS_WIDTH,
+        height: CANVAS_HEIGHT,
+        backgroundColor: "#808080",
+        selection: false
+      })
 
-    // -> Setup objects & its properties
-    const rectTop = new fabric.Rect({
-      fill: "yellow",
-      height: CANVAS_HEIGHT * 0.5,
-      width: CANVAS_WIDTH,
-      top: 0,
-      hoverCursor: "pointer",
-    }).set(OBJECT_LOCKED)
+      // 2. Setup objects & its properties
+      const rectTop = new fabric.Rect({
+        fill: "yellow",
+        height: CANVAS_HEIGHT * 0.5,
+        width: CANVAS_WIDTH,
+        top: 0,
+        hoverCursor: "pointer",
+      }).set(OBJECT_LOCKED)
 
-    const rectBottom = new fabric.Rect({
-      fill: "blue",
-      height: CANVAS_HEIGHT * 0.5,
-      width: CANVAS_WIDTH + 1,
-      top: CANVAS_HEIGHT + 1,
-      left: -1,
-      originY: "bottom",
-      hoverCursor: "pointer",
-    }).set(OBJECT_LOCKED)
+      const rectBottom = new fabric.Rect({
+        fill: "blue",
+        height: CANVAS_HEIGHT * 0.5,
+        width: CANVAS_WIDTH + 1,
+        top: CANVAS_HEIGHT + 1,
+        left: -1,
+        originY: "bottom",
+        hoverCursor: "pointer",
+      }).set(OBJECT_LOCKED)
 
-    canvas.add(rectTop)
-    canvas.add(rectBottom)
-    canvas.renderAll()
+      canvas.add(rectTop)
+      canvas.add(rectBottom)
+      canvas.renderAll()
 
-    // -> Clean up the canvas when the component unmounts
-    return () => {
-      canvas.dispose()
+      // 3. Clean up the canvas when the component unmounts
+      return () => {
+        canvas.dispose()
+      }
     }
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
