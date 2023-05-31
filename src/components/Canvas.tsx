ef732fe2da5f1c9be9509e353e08cce464fcb3ea
ricafolio <29043464+ricafolio@users.noreply.github.com>
2023-05-31 21:45:52 +0800

    feat: add working image upload

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -25,7 +25,7 @@ export default function Canvas() {
     if (canvasRef.current) {
       // 1. Setup canvas
       const canvas = new fabric.Canvas(canvasRef.current, {
-        backgroundColor: "#808080",
+        backgroundColor: "#232323",
         width: CANVAS_WIDTH,
         height: CANVAS_HEIGHT,
         selection: false,
@@ -50,11 +50,60 @@ export default function Canvas() {
         hoverCursor: "pointer",
       }).set(OBJECT_LOCKED)
 
+      // 2.1 Define image upload event handler
+      const handleImageUpload = (gridCell: fabric.Rect) => {
+        const input = document.createElement("input")
+        input.type = "file"
+        input.accept = "image/*"
+
+        input.onchange = async (event) => {
+            const target = event.target as HTMLInputElement
+            const file = target.files && target.files[0]
+            if (!file) return
+
+            const reader = new FileReader()
+            reader.readAsDataURL(file)
+            reader.onload = (e) => {
+              const dataUrl = e.target?.result as string
+              const addImage = async (imageBase64: string) => {
+                const img = await fabric.Image.fromURL(imageBase64)
+                img.set({
+                  left: gridCell.left,
+                  top: gridCell.top,
+                  selectable: true,
+                  hasControls: true,
+                  clipPath: new fabric.Rect({
+                    originY: "top",
+                    height: CANVAS_HEIGHT * 0.5,
+                    width: CANVAS_WIDTH,
+                  }),
+                })
+                canvas.add(img)
+              }
+              addImage(dataUrl)
+            }
+
+            canvas.remove(gridCell)
+            canvas.renderAll()
+        }
+
+        input.click()
+      }
+
+      // 3. Attach event handler
+      rectTop.on("mouseup", () => {
+        handleImageUpload(rectTop)
+      })
+      rectBottom.on("mouseup", () => {
+        handleImageUpload(rectBottom)
+      })
+
+      // 4. Render
       canvas.add(rectTop)
       canvas.add(rectBottom)
       canvas.renderAll()
 
-      // 3. Clean up the canvas when the component unmounts
+      // 5. Clean up the canvas when the component unmounts
       return () => {
         canvas.dispose()
       }
