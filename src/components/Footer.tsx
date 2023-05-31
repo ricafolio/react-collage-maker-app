8867a34fd72372aad53eba1c4efb2a3f4e9a40f5
ricafolio <29043464+ricafolio@users.noreply.github.com>
2023-05-31 15:05:46 +0800

    chore: remove download button on footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,10 +5,5 @@ export default function Footer() {
     <section className="w-full mt-4">
       <TabGroup />
     </section>
-    <section className="w-full mt-4">
-      <button className="w-full text-sm bg-blue-600 hover:bg-blue-700 transition-colors rounded font-semibold transition px-5 py-2">
-        Download image
-      </button>
-    </section>
   </>
 }
