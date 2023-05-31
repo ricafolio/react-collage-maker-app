6e66a6ca1ca506b9930b8ced719bd3f4c4637715
ricafolio <29043464+ricafolio@users.noreply.github.com>
2023-05-31 18:42:44 +0800

    chore: change header content

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
-    <div className="my-3 text-center">
-      <h1 className="text-sm font-bold uppercase">Collage Canvas - Photo Collage Maker in React.js</h1>
+    <div className="py-4 text-center">
+      <h1 className="text-sm font-semibold">React.js Collage Maker <a href="https://github.com/ricafolio/photo-app" target="_blank" className="text-gray-600 hover:text-gray-500">(Github repo)</a></h1>
     </div>
   )
 }
