77f48341e2d8eae50a6ff8c7687fe6186916e775
ricafolio <29043464+ricafolio@users.noreply.github.com>
2023-06-01 21:35:15 +0800

    chore: add aspect ratio helper library

---
+++ src/components/lib/aspectRatioHelper.ts
@@ -0,0 +1,8 @@
+import type { DimensionsType } from "@/types"
+
+export default function calculateHeightByAspectRatio(widthRatio: number, heightRatio: number, exactWidth: number): DimensionsType {
+  return {
+    width: exactWidth,
+    height: Math.round((exactWidth / widthRatio) * heightRatio),
+  }
+}
