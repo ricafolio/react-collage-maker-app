6bd3da5e5198de0cf2fe5a08276a3eb9802ab270
ricafolio <29043464+ricafolio@users.noreply.github.com>
2023-06-01 22:07:54 +0800

    chore: update canvas config

--- src/constants/canvasConfig.ts
+++ src/constants/canvasConfig.ts
@@ -1,4 +1,5 @@
 import { LockedObjectType, CollageTemplateType, AspectRatioType } from "@/types"
+import calculateHeightByAspectRatio from "@/components/lib/aspectRatioHelper"
 
 export const OBJECT_LOCKED: LockedObjectType = {
   lockMovementX: true,
@@ -43,52 +44,91 @@ export const COLLAGE_TEMPLATES: CollageTemplateType[] = [
       },
     ],
   },
+  {
+    id: 2,
+    uid: "three-landscape-photos",
+    name: "Three landscape photos",
+    config: [
+      {
+        rectFabric: (CANVAS_HEIGHT, CANVAS_WIDTH) => ({
+          fill: "#1a1a1a",
+          height: CANVAS_HEIGHT * (1 / 3) + 1,
+          width: CANVAS_WIDTH + 1,
+          top: -1,
+          left: -1,
+          absolutePositioned: true,
+          hoverCursor: "pointer",
+        }),
+      },
+      {
+        rectFabric: (CANVAS_HEIGHT, CANVAS_WIDTH) => ({
+          fill: "#2a2a2a",
+          height: CANVAS_HEIGHT * (1 / 3),
+          width: CANVAS_WIDTH + 1,
+          top: CANVAS_HEIGHT * (1 / 3),
+          left: -1,
+          absolutePositioned: true,
+          hoverCursor: "pointer",
+        }),
+      },
+      {
+        rectFabric: (CANVAS_HEIGHT, CANVAS_WIDTH) => ({
+          fill: "#3a3a3a",
+          height: CANVAS_HEIGHT * (1 / 3),
+          width: CANVAS_WIDTH + 1,
+          top: CANVAS_HEIGHT * (1 / 3) * 2,
+          left: -1,
+          absolutePositioned: true,
+          hoverCursor: "pointer",
+        }),
+      },
+    ],
+  },
 ]
+
+const DEFAULT_HEIGHT = 576
 
 export const ASPECT_RATIOS: AspectRatioType[] = [
   {
     id: 1,
     name: "1:1",
     nickname: "Square",
-    canvas: {
-      width: 576,
-      height: 576,
-    },
+    canvas: calculateHeightByAspectRatio(1, 1, DEFAULT_HEIGHT),
   },
   {
     id: 2,
     name: "3:2",
     nickname: "",
-    canvas: {
-      width: 1080,
-      height: 720,
-    },
+    canvas: calculateHeightByAspectRatio(3, 2, DEFAULT_HEIGHT),
   },
   {
     id: 3,
     name: "4:3",
     nickname: "",
-    canvas: {
-      width: 1024,
-      height: 768,
-    },
+    canvas: calculateHeightByAspectRatio(4, 3, DEFAULT_HEIGHT),
   },
   {
     id: 4,
     name: "16:9",
     nickname: "Cinematic",
-    canvas: {
-      width: 1200,
-      height: 628,
-    },
+    canvas: calculateHeightByAspectRatio(16, 9, DEFAULT_HEIGHT),
   },
   {
     id: 5,
     name: "9:16",
     nickname: "Tiktok",
-    canvas: {
-      width: 628,
-      height: 1200,
-    },
+    canvas: calculateHeightByAspectRatio(9, 16, DEFAULT_HEIGHT),
+  },
+  {
+    id: 6,
+    name: "2:3",
+    nickname: "",
+    canvas: calculateHeightByAspectRatio(2, 3, DEFAULT_HEIGHT),
+  },
+  {
+    id: 5,
+    name: "3:4",
+    nickname: "",
+    canvas: calculateHeightByAspectRatio(3, 4, DEFAULT_HEIGHT),
   },
 ]
