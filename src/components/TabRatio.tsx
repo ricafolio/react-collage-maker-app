44bb253d011399691d0af6efaf5b15905fc654e6
ricafolio <29043464+ricafolio@users.noreply.github.com>
2023-06-01 21:28:21 +0800

    feat: add ratio switcher

--- src/components/TabRatio.tsx
+++ src/components/TabRatio.tsx
@@ -1,5 +1,26 @@
+import { useAppSelector, useAppDispatch } from '@/redux/hooks'
+import { changeRatioByIndex } from '@/redux/settingsSlice'
+import { ASPECT_RATIOS } from "@/constants/canvasConfig"
+import { RootStateType } from '@/redux/store'
+
 export default function TabRatio() {
+  const activeRatioIndex = useAppSelector((state: RootStateType) => state.settings.ratio)
+  const dispatch = useAppDispatch()
+
   return (
-    <></>
+    <div className="grid grid-flow-col gap-x-2 text-white">
+      {ASPECT_RATIOS.map((ratio, index) => {
+        return (
+          <div 
+            key={`ratio-${index}`}
+            onClick={() => dispatch(changeRatioByIndex(index))}
+            className={`w-20 h-20 cursor-pointer flex flex-col justify-center items-center border transition-colors ${index === activeRatioIndex ? 'border-white' : 'border-neutral-800 hover:border-neutral-200'}`}
+          >
+            <span>{ratio.name}</span>
+            <small className="text-xs">{ratio.nickname}</small>
+          </div>
+        )
+      })}
+    </div>
   )
 }
