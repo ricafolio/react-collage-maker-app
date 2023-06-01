688596923cb97b2b180c6451d222849bb9255d1b
ricafolio <29043464+ricafolio@users.noreply.github.com>
2023-06-01 19:42:39 +0800

    chore: setup empty redux store

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,26 @@
 import Canvas from "@/components/Canvas"
 import Footer from "@/components/Footer"
 import Header from "@/components/Header"
+import { Provider } from "react-redux"
+import store from "@/redux/store"
 
 function App() {
   return (
-    <div className="mx-auto max-w-xl">
-      <header>
-        <Header />
-      </header>
+    <Provider store={store}>
+      <div className="mx-auto max-w-xl">
+        <header>
+          <Header />
+        </header>
 
-      <main>
-        <Canvas />
-      </main>
+        <main>
+          <Canvas />
+        </main>
 
-      <footer>
-        <Footer />
-      </footer>
-    </div>
+        <footer>
+          <Footer />
+        </footer>
+      </div>
+    </Provider>
   )
 }
 
