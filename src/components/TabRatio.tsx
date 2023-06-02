d04d1b348f323a8c90b83708138f16936198ccf2
ricafolio <29043464+ricafolio@users.noreply.github.com>
2023-06-02 17:01:10 +0800

    feat: display icon on ratio options

--- src/components/TabRatio.tsx
+++ src/components/TabRatio.tsx
@@ -21,14 +21,15 @@ export default function TabRatio() {
               dispatch(changeRatioByIndex(index)) 
               toast.success(`Ratio changed to ${ratio.name}`, { duration: 800 })
             }}
-            className={`flex h-20 w-20 px-2 cursor-pointer flex-col items-center justify-center transition-colors rounded ${
+            className={`flex h-20 w-20 cursor-pointer flex-col items-center justify-center text-center transition-colors rounded ${
               index === activeRatioIndex
                 ? "bg-neutral-800"
                 : "hover:bg-neutral-800"
             }`}
+            aria-label={`change aspect ratio to ${ratio.name}`}
           >
-            <span className="w-full text-center">{ratio.name}</span>
-            <small className="w-full text-center text-xs">{ratio.nickname}</small>
+            <img src={ratio.icon} alt={ratio.name} />
+            <span className="w-full text-center mt-1">{ratio.name}</span>
           </button>
         )
       })}
