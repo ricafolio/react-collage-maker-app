10ab0502339196f2cf8dd6f13264bd1778bad1e1
ricafolio <29043464+ricafolio@users.noreply.github.com>
2023-06-03 05:03:58 +0800

    feat: add noise filter control

--- src/components/TabMore.tsx
+++ src/components/TabMore.tsx
@@ -1,30 +1,77 @@
+import type { RootStateType } from "@/redux/store"
+import type { FilterType } from "@/types"
 import { useAppSelector } from "@/redux/hooks"
-import { RootStateType } from "@/redux/store"
-import { useRef } from "react"
+import { useState } from "react"
+import * as fabric from "fabric"
+import toast, { Toaster } from "react-hot-toast"
 
 export default function TabMore() {
-  const linkRef = useRef<HTMLAnchorElement | null>(null)
-  const clonedCanvas = useAppSelector(
+  const [setting, setSetting] = useState<FilterType>("Noise")
+  const [value, setValue] = useState<number>(0)
+
+  const canvas = useAppSelector(
     (state: RootStateType) => state.settings.canvas
   )
 
-  function handleClick() {
-    console.log(clonedCanvas)
+  function addFilter() {
+    // to do: add filter by id
+    if (canvas) {
+      const image = canvas.getActiveObject() as fabric.Image
+      if (image) {
+        const parsedImage = image.toJSON()
+        const filterIndex = parsedImage.filters.findIndex((f: { type: string, noise: number}) => f.type === 'Noise')
 
-    if (clonedCanvas && linkRef.current) {
-      clonedCanvas.discardActiveObject()
-      linkRef.current.href = clonedCanvas.toDataURL()
-      linkRef.current.download = `collage-${new Date().getTime()}.png`
-      linkRef.current.click()
+        if (filterIndex !== -1) {
+          // Change existing filter
+          (image.filters[filterIndex] as fabric.filters.Noise).noise = value
+        } else {
+          // Create new filter
+          const noiseFilter = new fabric.filters.Noise({
+            noise: 0
+          }) as fabric.filters.BaseFilter
+          image.filters.push(noiseFilter)
+        }
+
+        image.applyFilters()
+        canvas.renderAll()
+      } else {
+        toast("Please select an image to apply filter", { id: "no-image" })
+        setValue(0)
+      }
     }
   }
 
   return (
-    <div className="kek">
-      <button type="button" onClick={handleClick} className="bg-blue-600 p-5">
-        CLICK ME!
+    <div className="w-full px-2">
+      <Toaster />
+      <button 
+        type="button" 
+        onClick={() => setSetting("Noise")}
+        className={`flex h-16 w-16 cursor-pointer flex-col items-center justify-center text-center transition-colors rounded ${
+              setting === "Noise"
+                ? "bg-neutral-800"
+                : "hover:bg-neutral-800"
+            }`}
+      >
+        <h3>{value}</h3>
+        <small className="text-xs">Noise</small>
       </button>
-      <a ref={linkRef} id="download" className="hidden"></a>
+      <div className="w-full flex justify-center items-center">
+        {setting === "Noise" && (
+          <input 
+            type="range" 
+            min={0} 
+            max={100} 
+            step={1} 
+            value={value} 
+            onChange={(e) => {
+              setValue(parseInt(e.target.value))
+              addFilter()
+            }}
+            className="w-full my-2 mx-4"
+          />
+        )}
+      </div>
     </div>
   )
 }
