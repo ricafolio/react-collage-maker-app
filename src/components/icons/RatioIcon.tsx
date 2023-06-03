f99d50f57eb5d42957e7a67c3d8121049c263438
ricafolio <29043464+ricafolio@users.noreply.github.com>
2023-06-04 05:27:55 +0800

    chore: fix ratio icon dimensions

--- src/components/icons/RatioIcon.tsx
+++ src/components/icons/RatioIcon.tsx
@@ -4,8 +4,8 @@ export default function RatioIcon(props: SVGProps<SVGSVGElement>) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width="1em"
-      height="1em"
+      width="1.15em"
+      height="1.15em"
       viewBox="0 0 15 15"
       {...props}
     >
