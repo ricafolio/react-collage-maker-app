50e08a7122e2bc9d60518499b2db18d485c04e7a
ricafolio <29043464+ricafolio@users.noreply.github.com>
2023-06-06 02:04:18 +0800

    format: Canvas.tsx

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -4,40 +4,44 @@ import {
   COLLAGE_TEMPLATES,
 } from "@/constants/canvasConfig"
 import { useAppSelector, useAppDispatch } from "@/redux/hooks"
-import { RootStateType } from "@/redux/store"
-import * as fabric from "fabric"
-import { useEffect, useRef } from "react"
-import toast from "react-hot-toast"
-import { 
-  changeTab, 
-  setCanvas, 
-  increaseUploadCount, 
+import {
+  changeTab,
+  setCanvas,
+  increaseUploadCount,
   resetUploadCount,
   newImage,
   setSelectedImage,
-  clearSelectedImage, 
+  clearSelectedImage,
 } from "@/redux/settingsSlice"
+import { RootStateType } from "@/redux/store"
 import { CustomImageObject } from "@/types"
+import * as fabric from "fabric"
+import { useEffect, useRef } from "react"
+import toast from "react-hot-toast"
 
 export default function Canvas() {
   const canvasRef = useRef<HTMLCanvasElement | null>(null)
   const inputRef = useRef<HTMLInputElement | null>(null)
   const wrapperRef = useRef<HTMLDivElement | null>(null)
-  const dispatch = useAppDispatch()
 
+  // Get necessary data from Redux store
+  const dispatch = useAppDispatch()
   const activeTemplateIndex = useAppSelector(
     (state: RootStateType) => state.settings.template
   )
   const activeRatioIndex = useAppSelector(
     (state: RootStateType) => state.settings.ratio
   )
-
   const activeTemplate = COLLAGE_TEMPLATES[activeTemplateIndex]
 
+  // Canvas initialization
   useEffect(() => {
     if (canvasRef.current && wrapperRef.current) {
       // 0. Calculate canvas ratio by initial client width
-      const panelWidth = wrapperRef.current.clientWidth > 640 ? 640 : wrapperRef.current.clientWidth - 16
+      const panelWidth =
+        wrapperRef.current.clientWidth > 640
+          ? 640 // fixed 640px canvas on >640px devices
+          : wrapperRef.current.clientWidth - 16 // 16px margin
       const ratio = ASPECT_RATIOS[activeRatioIndex].canvas(panelWidth)
 
       // 1. Setup canvas
@@ -59,10 +63,7 @@ export default function Canvas() {
 
       // 2. Setup objects & its properties
       activeTemplate.config.forEach((config) => {
-        const PROPERTIES = config.rectFabric(
-          ratio.height,
-          ratio.width
-        )
+        const PROPERTIES = config.rectFabric(ratio.height, ratio.width)
         const cell = new fabric.Rect(PROPERTIES).set(OBJECT_LOCKED)
 
         // 3. Define image upload event handler
@@ -74,12 +75,12 @@ export default function Canvas() {
               const file = target.files && target.files[0]
               if (!file) return
 
-              // 1. Load uploaded file as Base64
+              // Load uploaded file as Base64
               const reader = new FileReader()
               reader.readAsDataURL(file)
               reader.onload = (e) => {
                 const dataUrl = e.target?.result as string
-                // 1.1 Load image as fabric image
+                // Load image as fabric image
                 const addImage = async (imageBase64: string) => {
                   const img = await fabric.Image.fromURL(imageBase64)
                   const imgId = `img_${new Date().getTime()}`
@@ -94,7 +95,7 @@ export default function Canvas() {
                     clipPath: selectedCell,
                     perPixelTargetFind: true,
                   }) as CustomImageObject
-                  
+
                   // Scale accordingly to look good
                   if (config.scaleTo === "width") {
                     img.scaleToWidth(selectedCell.width + 1)
@@ -102,18 +103,20 @@ export default function Canvas() {
                     img.scaleToHeight(selectedCell.height + 1)
                   }
 
-                  // save image in redux
-                  dispatch(newImage({
-                    id: imgId,
-                    filters: {
-                      brightness: 0,
-                      contrast: 0,
-                      noise: 0,
-                      saturation: 0,
-                      vibrance: 0,
-                      blur: 0,
-                    }
-                  }))
+                  // Save image in redux
+                  dispatch(
+                    newImage({
+                      id: imgId,
+                      filters: {
+                        brightness: 0,
+                        contrast: 0,
+                        noise: 0,
+                        saturation: 0,
+                        vibrance: 0,
+                        blur: 0,
+                      },
+                    })
+                  )
 
                   canvas.add(img)
                   canvas.setActiveObject(img)
@@ -124,8 +127,10 @@ export default function Canvas() {
               // Render in canvas
               canvas.remove(selectedCell)
               canvas.renderAll()
-              toast.success("Image successfully added.", { id: "toast-uploaded" })
-              
+              toast.success("Image successfully added.", {
+                id: "toast-uploaded",
+              })
+
               // Increment upload count
               dispatch(increaseUploadCount())
 
@@ -181,16 +186,30 @@ export default function Canvas() {
 
   return (
     <div ref={wrapperRef}>
-      <div className="py-2 sm:py-8 sm:min-h-screen flex items-start justify-center">
+      <div className="flex items-start justify-center py-2 sm:min-h-screen sm:py-8">
         <canvas ref={canvasRef} />
       </div>
       <div className="hidden">
         <input ref={inputRef} type="file" accept="image/*" className="hidden" />
       </div>
-      <div className="w-full text-center hidden sm:block">
-        <a href="https://github.com/ricafolio/react-canvas-collage-maker/" target="_blank" rel="noopener noreferrer" className="text-gray-600 hover:text-gray-200 transition-colors font-medium">Source code</a>
+      <div className="hidden w-full text-center sm:block">
+        <a
+          href="https://github.com/ricafolio/react-canvas-collage-maker/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="font-medium text-gray-600 transition-colors hover:text-gray-200"
+        >
+          Source code
+        </a>
         <span className="mx-3 text-gray-700">&middot;</span>
-        <a href="https://ricafolio.me" target="_blank" rel="noopener noreferrer" className="text-gray-600 hover:text-gray-200 transition-colors font-medium">ricafolio.me</a>
+        <a
+          href="https://ricafolio.me"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="font-medium text-gray-600 transition-colors hover:text-gray-200"
+        >
+          ricafolio.me
+        </a>
       </div>
     </div>
   )
