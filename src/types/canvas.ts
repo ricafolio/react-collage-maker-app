75e0158569dc6cff38bb6b90f1043f1cefcc5bcb
ricafolio <29043464+ricafolio@users.noreply.github.com>
2023-06-06 02:45:38 +0800

    refactor: group types into separate files

---
+++ src/types/canvas.ts
@@ -0,0 +1,43 @@
+import type { Canvas } from "fabric"
+
+export type SelectedTabType = "template" | "ratio" | "more"
+
+export type RectFabricFunctionType = (CANVAS_HEIGHT: number, CANVAS_WIDTH: number) => {
+  fill: string
+  height: number
+  width: number
+  absolutePositioned: boolean
+  hoverCursor: string
+  top?: number
+  left?: number
+}
+
+export type RectConfigType = {
+  rectFabric: RectFabricFunctionType
+  scaleTo: "width" | "height"
+}
+
+export type CollageTemplateType = {
+  name: string
+  icon: string
+  config: RectConfigType[]
+}
+
+export type DimensionsType = {
+  width: number
+  height: number
+}
+
+export type AspectRatioType = {
+  name: string
+  nickname: string
+  icon: string
+  canvas: (w: number) => DimensionsType
+}
+
+export interface CanvasStateType {
+  canvas: Canvas | null
+  ratio: number
+  template: number
+  tab: SelectedTabType
+}
---
+++ src/types/objects.ts
@@ -0,0 +1,12 @@
+export type LockedObjectType = {
+  lockMovementX: boolean
+  lockMovementY: boolean
+  lockRotation: boolean
+  lockScalingFlip: boolean
+  lockScalingX: boolean
+  lockScalingY: boolean
+  lockSkewingX: boolean
+  lockSkewingY: boolean
+  hasControls: boolean
+  selectable: boolean
+}
