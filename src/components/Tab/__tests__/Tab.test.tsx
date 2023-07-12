03b1153d0727e937f6b696cbd0631eae9adf301e
ricafolio <29043464+ricafolio@users.noreply.github.com>
2023-07-13 01:58:28 +0800

    chore: move filter tests

--- src/components/Tab/__tests__/Tab.test.tsx
+++ src/components/Tab/__tests__/Tab.test.tsx
@@ -1,6 +1,5 @@
 import TabPanel from "@/components/Tab/TabPanel"
 import store from "@/redux/store"
-import { FilterIdType } from "@/types"
 
 import { render, screen, cleanup, fireEvent } from "@testing-library/react"
 import { Provider } from "react-redux"
@@ -84,55 +83,5 @@ describe("Tabs content renders correctly", () => {
 
   it("Ratio should show 8 options", () => {
     assertTabContent("ratio", 8)
-  })
-
-  it("Filters should show 6 controls", () => {
-    const filterId: FilterIdType[] = [
-      "Brightness",
-      "Contrast",
-      "Noise",
-      "Saturation",
-      "Vibrance",
-      "Blur"
-    ]
-
-    // mock window
-    Object.defineProperty(window, 'matchMedia', {
-      value: () => ({
-        matches: false,
-      })
-    })
-
-    // find filter tab button
-    const buttonText = screen.queryByText("filters")
-    const buttonElement = buttonText?.closest("button")
-
-    if (buttonElement) {
-      // Click filter tab
-      fireEvent.click(buttonElement)
-
-      // Get filter tab content
-      const tabContentElement = screen.getByTestId("tabContent")
-
-      // Should have 6 input element
-      const filterSliderInput = tabContentElement.querySelectorAll("input")
-      expect(filterSliderInput).toHaveLength(filterId.length)
-
-      filterSliderInput.forEach((input) => {
-        // Should have range type
-        expect(input).toHaveProperty("type")
-        expect(input.type).toBe("range")
-
-        // Should have these properties
-        expect(input).toHaveProperty("id")
-        expect(input).toHaveProperty("min")
-        expect(input).toHaveProperty("max")
-        expect(input).toHaveProperty("step")
-        expect(input).toHaveProperty("value")
-
-        // Should have one of the filter id
-        expect(filterId.includes(input.id as FilterIdType)).toBe(true)
-      })
-    }
   })
 })
