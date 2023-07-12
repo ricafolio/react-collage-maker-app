0d9d4466bac8156a5f158afa8ff45c7e155e33af
ricafolio <29043464+ricafolio@users.noreply.github.com>
2023-07-13 01:36:14 +0800

    chore: add filter tab content test

--- src/components/Tab/__tests__/Tab.test.tsx
+++ src/components/Tab/__tests__/Tab.test.tsx
@@ -1,5 +1,6 @@
 import TabPanel from "@/components/Tab/TabPanel"
 import store from "@/redux/store"
+import { FilterIdType } from "@/types"
 
 import { render, screen, cleanup, fireEvent } from "@testing-library/react"
 import { Provider } from "react-redux"
@@ -15,8 +16,10 @@ function assertButtonWithText(text: string) {
 }
 
 function assertTabContent(tab: string, numberOfButtons: number) {
+  // find template / ratio button
   const buttonText = screen.queryByText(tab)
   const buttonElement = buttonText?.closest("button")
+  
   if (buttonElement) {
     // Click Template tab
     fireEvent.click(buttonElement)
@@ -25,7 +28,7 @@ function assertTabContent(tab: string, numberOfButtons: number) {
     const tabContentElement = screen.getByTestId("tabContent")
     const templateButtons = tabContentElement.querySelectorAll("button")
 
-    // Should have 10 templates
+    // Should have X templates
     expect(templateButtons).toHaveLength(numberOfButtons)
 
     // Should have icons inside buttons
@@ -76,10 +79,60 @@ describe("Tabs content renders correctly", () => {
   })
 
   it("Template should show 10 options", () => {
-    assertTabContent("Template", 10)
+    assertTabContent("template", 10)
   })
 
   it("Ratio should show 8 options", () => {
-    assertTabContent("Ratio", 8)
+    assertTabContent("ratio", 8)
+  })
+
+  it("Filters should show 6 controls", () => {
+    const filterId: FilterIdType[] = [
+      "Brightness",
+      "Contrast",
+      "Noise",
+      "Saturation",
+      "Vibrance",
+      "Blur"
+    ]
+
+    // mock window
+    Object.defineProperty(window, 'matchMedia', {
+      value: () => ({
+        matches: false,
+      })
+    })
+
+    // find filter tab button
+    const buttonText = screen.queryByText("filters")
+    const buttonElement = buttonText?.closest("button")
+
+    if (buttonElement) {
+      // Click filter tab
+      fireEvent.click(buttonElement)
+
+      // Get filter tab content
+      const tabContentElement = screen.getByTestId("tabContent")
+
+      // Should have 6 input element
+      const filterSliderInput = tabContentElement.querySelectorAll("input")
+      expect(filterSliderInput).toHaveLength(filterId.length)
+
+      filterSliderInput.forEach((input) => {
+        // Should have range type
+        expect(input).toHaveProperty("type")
+        expect(input.type).toBe("range")
+
+        // Should have these properties
+        expect(input).toHaveProperty("id")
+        expect(input).toHaveProperty("min")
+        expect(input).toHaveProperty("max")
+        expect(input).toHaveProperty("step")
+        expect(input).toHaveProperty("value")
+
+        // Should have one of the filter id
+        expect(filterId.includes(input.id as FilterIdType)).toBe(true)
+      })
+    }
   })
 })
